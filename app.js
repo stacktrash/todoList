4100dde8e440f43a0f2d0f1651195b68b5182a16
vmazz <vdogmark7@gmail.com>
2023-07-16 18:37:43 -0400

    input field/ post request

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.set('view engine', 'ejs'); //use ejs as view engine
-
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get("/", function(req, res) {
   var today = new Date();
-  var options = {weekday: "long",
-  day: "numeric",
-  month: "long"};
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-   var day = today.toLocaleDateString("en-US", options)//options sets our format
+  var day = today.toLocaleDateString("en-US", options) //options sets our format
 
 
 
@@ -22,6 +26,11 @@ app.get("/", function(req, res) {
   }) //renders list file, sends day to KindofDay variabke
 
 });
+
+app.post("/", function(req, res) {
+  var item = req.body.newItem
+  console.log(item)
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000.");
