0acacc9028586306c5c9d92ef2faf769f985a8f9
vmazz <vdogmark7@gmail.com>
2023-07-17 23:10:47 -0400

    add work page template

--- app.js
+++ app.js
@@ -3,12 +3,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-var items = []; //array that holds new items. w/o this we just rewrite the last item in the list
+var items = ["make bed"]; //array that holds new items. w/o this we just rewrite the last item in the list
+var workItems = [];
 const app = express();
 app.set('view engine', 'ejs'); //use ejs as view engine
 app.use(bodyParser.urlencoded({
-  extended: true
-}));
+  extended: true}));
+
+app.use(express.static('public')); //since express cannot load css from index.html, we have to resort to this
 
 app.get("/", function(req, res) {
   var today = new Date();
@@ -23,16 +25,37 @@ app.get("/", function(req, res) {
 
 
   res.render("list", {
-    KindofDay: day, newListItem: items
+    listTitle: day,
+    newListItems: items
   }) //The res.render() function is used to render a view and sends the rendered HTML string to the client.
 
 });
+app.post("/", function(req, res){
+  console.log(req.body);
+  let item = req.body.newItem;
 
-app.post("/", function(req, res) {
-  var item = req.body.newItem
-  items.push(item); //pushes item to list every refresh
-  res.redirect("/"); //redirects us to homepage to refresh for our new item
+
+  items.push(item);
+
+  res.redirect("/");
 })
+
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems})
+})
+
+app.post("/work", function(req, res){
+  let item = req.body.newItem;
+  workItems.push(item)
+  res.redirect("/work")
+
+})
+
+// app.post("/", function(req, res) {
+//   var item = req.body.newItem
+//   items.push(item); //pushes item to list every refresh
+//   res.redirect("/"); //redirects us to homepage to refresh for our new item
+// })
 
 app.listen(3000, function() {
   console.log("Server started on port 3000.");
